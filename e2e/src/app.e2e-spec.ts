bab4ad8b51fcef154f41e2066eb9ba4be8f5862a
Ngô Lê Thiên Ân <quocthienhong@gmail.com>
2024-08-22 10:21:57 +0700

    push foodmine

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', async () => {
     await page.navigateTo();
-    expect(await page.getTitleText()).toEqual('foodmine app is running!');
+    expect(await page.getTitleText()).toEqual('foodmine-course app is running!');
   });
 
   afterEach(async () => {
